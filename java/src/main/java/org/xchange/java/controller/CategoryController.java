5f227e42173a5f079085e5a8987121a6f1900d89
Lilecaz <lilecaz0090@gmail.com>
2025-03-06 15:34:01 +0100

    fix errors

---
+++ java/src/main/java/org/xchange/java/controller/CategoryController.java
@@ -0,0 +1,68 @@
+package org.xchange.java.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.xchange.java.model.Category;
+import org.xchange.java.model.ExchangeObject;
+import org.xchange.java.service.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+            return ResponseEntity.ok(category.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category createdCategory = categoryService.createCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Category updatedCategory = categoryService.updateCategory(id, category);
+        if (updatedCategory != null) {
+            return ResponseEntity.ok(updatedCategory);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+    /*
+    @GetMapping("/{id}/objects")
+    public ResponseEntity<List<ExchangeObject>> getObjectsByCategoryId(@PathVariable Long id) {
+        List<ExchangeObject> exchangeObjects = categoryService.getObjectsByCategoryId(id);
+        return ResponseEntity.ok(exchangeObjects);
+    }*/
+}
--- java/src/main/java/org/xchange/java/model/Exchange.java
+++ java/src/main/java/org/xchange/java/model/Exchange.java
@@ -1,66 +1,39 @@
 package org.xchange.java.model;
 
-import jakarta.persistence.*;
+   import jakarta.persistence.*;
 
+   @Entity
+   public class Exchange {
+       @Id
+       @GeneratedValue(strategy = GenerationType.IDENTITY)
+       private Long id;
+       private String status;
 
-@Entity
-public class Exchange {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String status;
+       @ManyToOne
+       @JoinColumn(name = "proposed_object_id", nullable = false)
+       private ExchangeObject proposedObject;
 
-    @ManyToOne
-    @JoinColumn(name = "proposed_object_id")
-    private ExchangeObject proposedExchangeObject;
+       @ManyToOne
+       @JoinColumn(name = "requested_object_id", nullable = false)
+       private ExchangeObject requestedObject;
 
-    @ManyToOne
-    @JoinColumn(name = "requested_object_id")
-    private ExchangeObject requestedExchangeObject;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-    // Getters et setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public ExchangeObject getProposedObject() {
-        return proposedExchangeObject;
-    }
-
-    public void setProposedObject(ExchangeObject proposedExchangeObject) {
-        this.proposedExchangeObject = proposedExchangeObject;
-    }
-
-    public ExchangeObject getRequestedObject() {
-        return requestedExchangeObject;
-    }
-
-    public void setRequestedObject(ExchangeObject requestedExchangeObject) {
-        this.requestedExchangeObject = requestedExchangeObject;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-    // Méthodes métier
-}
+       // Getters and setters
+       public Long getId() {
+           return id;
+       }
+       public void setId(Long id) {
+           this.id = id;
+       }
+       public ExchangeObject getProposedObject() {
+           return proposedObject;
+       }
+       public void setProposedObject(ExchangeObject proposedObject) {
+           this.proposedObject = proposedObject;
+       }
+       public ExchangeObject getRequestedObject() {
+           return requestedObject;
+       }
+       public void setRequestedObject(ExchangeObject requestedObject) {
+           this.requestedObject = requestedObject;
+       }
+   }
